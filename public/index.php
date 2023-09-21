bca0f687a583ce62a23bf7f0b3821dfb1f359794
Junaid Khan <mrjunaid444@gmail.com>
2023-09-21 20:45:56 +0530

    code formated

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+define("LARAVEL_START", microtime(true));
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . "/../storage/framework/maintenance.php")) {
     require $maintenance;
 }
 
@@ -26,12 +26,12 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
+| this application. We just need to utilize it! We"ll simply require it
+| into the script here so we don"t need to manually load our classes.
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . "/../vendor/autoload.php";
 
 /*
 |--------------------------------------------------------------------------
@@ -39,12 +39,12 @@ require __DIR__.'/../vendor/autoload.php';
 |--------------------------------------------------------------------------
 |
 | Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+| the application"s HTTP kernel. Then, we will send the response back
+| to this client"s browser, allowing them to enjoy our application.
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . "/../bootstrap/app.php";
 
 $kernel = $app->make(Kernel::class);
 
