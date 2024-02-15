a5305f724a4a1244e1260d2dbb9b2e9f1912a74a
Junaid Khan <mrjunaid444@gmail.com>
2024-02-15 22:09:45 +0530

    :art: Code formatted in public, resource and routes

--- routes/api.php
+++ routes/api.php
@@ -12,24 +12,24 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the 'api' middleware group. Make something great!
 |
 */
 
-Route::prefix("auth")
+Route::prefix('auth')
     ->controller(AuthController::class)
     ->group(function () {
-        Route::post("/sign-up", [AuthController::class, "signUp"]);
-        Route::post("/sign-in", [AuthController::class, "signIn"]);
-        Route::middleware("auth:sanctum")
-            ->post("/sign-out", [AuthController::class, "signOut"]);
+        Route::post('/sign-up', [AuthController::class, 'signUp']);
+        Route::post('/sign-in', [AuthController::class, 'signIn']);
+        Route::middleware('auth:sanctum')
+            ->post('/sign-out', [AuthController::class, 'signOut']);
     });
 
-Route::middleware("auth:sanctum")->group(function () {
-    Route::prefix("profile")
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('profile')
         ->controller(ProfileController::class)
         ->group(function () {
-            Route::get("/", "getProfile");
-            Route::post("/", "updateProfile");
+            Route::get('/', 'getProfile');
+            Route::post('/', 'updateProfile');
         });
 });
